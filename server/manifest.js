b6b1edbc282d677d6574ec5a83c848c015b9ee7b
Axel Salem <axelsalem.dev@gmail.com>
2023-04-01 00:22:58 +0200

    Optionnal Refactoring

--- server/manifest.js
+++ server/manifest.js
@@ -3,8 +3,8 @@
 const Dotenv = require('dotenv');
 const Confidence = require('@hapipal/confidence');
 const Toys = require('@hapipal/toys');
-const Schwifty = require('@hapipal/schwifty');
 
+require('@hapipal/schwifty');
 // Pull .env into process.env
 Dotenv.config({ path: `${__dirname}/.env` });
 
